d6b4194e9c550a0c0579d6ef6809499d9442dde7
mbarnekow <mbarnekow46@gmail.com>
2019-04-04 18:10:36 -0700

    First commit of TDD test repo

---
+++ test.c
@@ -0,0 +1,10 @@
+/* test file for TDD with unity*/
+#include <stdio.h>
+
+int
+main (int argc, char ** argv)
+{
+    printf("hello world\n");
+    printf("argument #=%i, first argument=%s\n", argc, *argv);
+    return 1;
+}
